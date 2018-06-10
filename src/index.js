7b697ee4332ebf8ec18639a47c7de4f22ab8afdf
Kamil <kamil_p7@o2.pl>
2018-06-10 13:35:22 +0200

    setting up Webpack for React

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <h3>Test</h3>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
