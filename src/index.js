c492d3e627cf7bde13c2d017d9983094ef5ccce3
Kamil <kamil_p7@o2.pl>
2018-06-13 18:09:43 +0200

    removing commented out lines

--- src/index.js
+++ src/index.js
@@ -8,32 +8,14 @@ import resultReducer from './store/reducers/result';
 import { createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 
-
 const rootReducer = combineReducers({
     input: inputReducer,
-   // res: inputReducer
     result: resultReducer
 });
-
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore( rootReducer, composeEnhancers(applyMiddleware(thunk)));
-
-
-
-
-// class App extends Component {
-//     render() {
-//         return (
-//             <div className="container">
-//                 <Calculator/>
-//             </div>
-//         )
-//     }
-// }
-
-// ReactDOM.render(<App />, document.getElementById('root'));
 
 ReactDOM.render(
     <Provider store={store}> 
