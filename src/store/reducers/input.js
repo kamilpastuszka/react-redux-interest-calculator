7e6d18f2af5b1b79a29e9aecd84fba05264096c8
Kamil <kamil_p7@o2.pl>
2018-10-13 13:01:16 +0100

    styles changes

--- src/store/reducers/input.js
+++ src/store/reducers/input.js
@@ -1,32 +1,31 @@
-import * as actionTypes from '../actions/actionTypes';
+import * as actionTypes from "../actions/actionTypes";
 
 const initialState = {
-    amount: '2',
-    interest: '4',
-    years: '1'
+  amount: "10000",
+  interest: "10",
+  years: "1"
+};
+
+const input = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_AMOUNT:
+      return {
+        ...state,
+        amount: action.amount
+      };
+    case actionTypes.SET_INTEREST:
+      return {
+        ...state,
+        interest: action.interest
+      };
+    case actionTypes.SET_YEARS:
+      return {
+        ...state,
+        years: action.years
+      };
+    default:
+      return state;
   }
-  
-  
-  const input = (state = initialState, action) => {
-    switch (action.type) {
-      case actionTypes.SET_AMOUNT:
-        return {
-          ...state,
-          amount: action.amount
-        }
-      case actionTypes.SET_INTEREST:
-        return {
-          ...state,
-          interest: action.interest
-        }
-      case actionTypes.SET_YEARS:
-        return {
-          ...state,
-          years: action.years
-        }
-      default:
-        return state
-    }
-  }
-  
-  export default input;
+};
+
+export default input;
